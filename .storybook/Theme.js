dce59da9c043772bf8931f2d355f2b846f41bdec
Jon McClure <jon.r.mcclure@gmail.com>
2022-08-11 16:00:51 +0100

    docs

--- .storybook/Theme.js
+++ .storybook/Theme.js
@@ -2,8 +2,8 @@ import { create } from '@storybook/theming';
 
 export default create({
   base: 'light',
-  brandTitle: 'Graphics svelte components',
-  brandUrl: 'https://reuters-graphics.github.io/graphics-svelte-components/',
+  brandTitle: 'Reuters Graphics components',
+  brandUrl: 'https://reuters-graphics.github.io/graphics-components/',
   brandImage: 'https://graphics.thomsonreuters.com/style-assets/images/logos/reuters-graphics-logo/svg/graphics-logo-color-dark.svg',
   brandTarget: '_self',
 });
