cee70532d3180390f7700213738632c6f1bf7bef
Jon McClure <jon.r.mcclure@gmail.com>
2022-08-26 20:27:47 +0100

    docs

--- src/components/Video/Video.svelte
+++ src/components/Video/Video.svelte
@@ -163,9 +163,9 @@
                   on:pausePlayEvent="{pausePlayEvent}"
                   paused="{paused}"
                   clickedOnPauseBtn="{clickedOnPauseBtn}"
-                  controlsOpacity="{hoverToSeeControls ?
-                    interactiveControlsOpacity :
-                    controlsOpacity}"
+                  controlsOpacity="{hoverToSeeControls
+                    ? interactiveControlsOpacity
+                    : controlsOpacity}"
                   controlsPosition="{controlsPosition}"
                   widthVideoContainer="{widthVideoContainer}"
                   heightVideoContainer="{heightVideoContainer}"
@@ -196,9 +196,9 @@
               bind:paused
               bind:clientWidth="{widthVideo}"
               bind:clientHeight="{heightVideo}"
-              style="{showControls ?
-                'position: relative' :
-                'position: relative'}"
+              style="{showControls
+                ? 'position: relative'
+                : 'position: relative'}"
             >
               <track kind="captions" />
             </video>
