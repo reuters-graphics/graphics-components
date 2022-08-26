a34edbbe3e8052315512c9004d1554666b144fae
Jon McClure <jon.r.mcclure@gmail.com>
2022-08-26 17:37:50 +0100

    SEO default location for origin

--- src/components/SEO/SEO.svelte
+++ src/components/SEO/SEO.svelte
@@ -80,6 +80,9 @@
     try {
       return new URL(baseUrl).origin;
     } catch {
+      // This handles a weird case where Vite's base path is
+      // reset to './' after the app hydrates...
+      if (typeof window !== 'undefined') return getOrigin(window.location.href);
       return '';
     }
   };
