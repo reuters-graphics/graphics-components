ad61eb558f5e60a3b7ac8ff9cfcd782a4267b5b3
Jon McClure <jon.r.mcclure@gmail.com>
2022-09-12 22:47:59 +0100

    suggested changes

---
+++ src/components/SiteHeadline/SiteHeadline.stories.svelte
@@ -0,0 +1,66 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+
+  // @ts-ignore
+  import componentDocs from './stories/docs/component.md?raw';
+  // @ts-ignore
+  import quickitDocs from './stories/docs/quickit.md?raw';
+
+  import SiteHeadline from './SiteHeadline.svelte';
+
+  import {
+    withComponentDocs,
+    withStoryDocs,
+  } from '$lib/docs/utils/withParams.js';
+
+  const meta = {
+    title: 'Components/SiteHeadline',
+    component: SiteHeadline,
+    ...withComponentDocs(componentDocs),
+  };
+
+  const content = {
+    Section: 'Global News',
+    SectionUrl: '',
+    Hed: 'A beautiful page',
+    Authors: 'Samuel Granados, Dea Bankova',
+    Published: '2022-09-12T08:30:00.000Z',
+    Updated: '',
+  };
+</script>
+
+<Meta {...meta} />
+
+<Template let:args>
+  <SiteHeadline {...args} />
+</Template>
+
+<Story
+  name="Default"
+  args="{{
+    section: 'Graphics',
+    sectionUrl: 'https://graphics.reuters.com',
+    hed: 'Ukraine makes surprising gains in swift counteroffensive',
+    authors: [
+      'Dea Bankova',
+      'Michael Ovaska',
+      'Samuel Granados',
+      'Prasanta Kumar Dutta',
+    ],
+    publishTime: new Date('2021-09-12').toISOString(),
+    updateTime: new Date('2021-09-12T13:57:00').toISOString(),
+  }}"
+/>
+
+<Story name="🚀 QUICKIT" {...withStoryDocs(quickitDocs)}>
+  <SiteHeadline
+    hed="{content.Hed}"
+    section="{content.Section}"
+    sectionUrl="{content.SectionUrl}"
+    authors="{content.Authors.split(',')}"
+    publishTime="{content.Published}"
+  />
+</Story>
+
+<style lang="scss">
+</style>
---
+++ src/components/SiteHeadline/stories/docs/component.md
@@ -0,0 +1,11 @@
+A simplified Reuters Graphics headline, loosely modelled off our dotcom site.
+
+Styles for this headline are intentionally restricted. It is meant to serve as a unifying style for quick-turnaround, usually shorter breaking news pages.
+
+```svelte
+<script>
+  import { SiteHeadline } from '@reuters-graphics/graphics-components';
+</script>
+
+<SiteHeadline hed="{'Reuters Graphics Interactive'}" />
+```
