fbea3a560f8caf4727127b62e373a50b9a1f2f10
Jon McClure <jon.r.mcclure@gmail.com>
2023-05-28 22:40:23 +0100

    types and cleanup

--- src/components/Analytics/providers/ga.ts
+++ src/components/Analytics/providers/ga.ts
@@ -1,38 +1,34 @@
 // Reuters Google Tag ID
 const GOOGLE_TAG_ID = 'G-W3Q2X6NTNM';
 
-/* eslint-disable */
+const URL = `https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}`;
+
 const attachScript = () => {
   // If script is already attached, skip
-  if (
-    document.querySelector(
-      `script[src="https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}"]`
-    )
-  )
-    return;
+  if (document.querySelector(`script[src="${URL}"]`)) return;
   // ... else attach it.
   const e = document.createElement('script');
   const n = document.getElementsByTagName('script')[0];
   e.type = 'text/javascript';
   e.async = true;
-  e.src = `https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}`;
+  e.src = URL;
   n.parentNode.insertBefore(e, n);
 };
-/* eslint-enable */
 
 export default () => {
   try {
-    // @ts-ignore
     window.dataLayer = window.dataLayer || [];
     if (!window.gtag) {
       attachScript();
       /** @type {Gtag.Gtag} */
       window.gtag = (...args) => {
-        // @ts-ignore
         window.dataLayer.push(...args);
       };
       window.gtag('js', new Date());
-      window.gtag('config', GOOGLE_TAG_ID);
+      // config event registers a pageview by default
+      window.gtag('config', GOOGLE_TAG_ID, {
+        page_location: window.location.origin + window.location.pathname,
+      });
     }
   } catch (e) { console.warn(`Error initialising Google Analytics: ${e}`); }
 };
