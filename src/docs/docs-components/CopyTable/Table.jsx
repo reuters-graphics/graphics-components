444d8b652bab3ea020c38ab40d78db12a9157ab9
Jon McClure <jon.r.mcclure@gmail.com>
2023-07-11 15:40:57 +0100

    nicer copy flag in CopyTable

--- src/docs/docs-components/CopyTable/Table.jsx
+++ src/docs/docs-components/CopyTable/Table.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 import { Unstyled } from '@storybook/blocks';
 // @ts-ignore
 import classes from './styles.module.scss';
 
-const handleClick = async(props) => {
-  const copyText = typeof props.copyable[props.column] === 'function' ?
-    props.copyable[props.column](`${props.children}`) : `${props.children}`;
-  await navigator.clipboard.writeText(copyText);
-}
-
 const MultiLine = (props) => props.children.split('\n').map(t => (<div>{t}</div>))
 
-const Copyable = (props) => props.copyable && props.copyable[props.column] ?
-  <button onClick={() => handleClick(props)}>
+const Copyable = (props) => {
+  const handleClick = async(props) => {
+    const copyText = typeof props.copyable[props.column] === 'function' ?
+      props.copyable[props.column](`${props.children}`) : `${props.children}`;
+    await navigator.clipboard.writeText(copyText);
+    setCopied(true);
+  }
+
+  const [copied, setCopied] = useState(false);
+
+  let timeout;
+
+  useEffect(() => {
+    if(timeout) clearTimeout(timeout);
+    timeout = setTimeout(() => { setCopied(false); }, 1000);
+  }, [copied]);
+
+  return props.copyable && props.copyable[props.column] ?
+  <button className="copy-btn" onClick={() => handleClick(props)}>
     <span className="material-symbols-outlined">content_copy</span>{props.children}
+    {copied && <div className="copy-tag">Copied</div>}
   </button> :
   <MultiLine>{props.children}</MultiLine>;
-const TD = (props) => <td><Copyable {...props}>{props.children}</Copyable></td>
+}
+
+  const TD = (props) => <td><Copyable {...props}>{props.children}</Copyable></td>
 const TR = (props) => <tr>{props.children.map((c, i) => (<TD {...props} column={i}>{c}</TD>))}</tr>
 const TH = (props) => <th>{props.children}</th>;
 
