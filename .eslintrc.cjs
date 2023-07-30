4ef43b925f6715fc0c72ede48b8d933b7622828e
Jon McClure <jon.r.mcclure@gmail.com>
2023-07-30 13:22:08 +0100

    switch indentation

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,7 +20,7 @@ module.exports = {
     'svelte3/typescript': require('typescript'),
   },
   rules: {
-    indent: ['error', 2],
+    indent: ['error', 2, { 'SwitchCase': 1 }],
     semi: ['error', 'always'],
     'comma-dangle': [
       'error',
