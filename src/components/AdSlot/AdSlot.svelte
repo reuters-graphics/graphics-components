be607960734033d404ac5d9dd90722477327b55d
Jon McClure <jon.r.mcclure@gmail.com>
2024-01-23 11:40:21 +0000

    adds extra adQueue params

--- src/components/AdSlot/AdSlot.svelte
+++ src/components/AdSlot/AdSlot.svelte
@@ -3,14 +3,16 @@
 
   type PlacementName =
     | 'reuters_desktop_leaderboard_atf'
-    | 'reuters_desktop_native'
-    | 'reuters_desktop_canvas';
+    | 'reuters_mobile_leaderboard'
+    | 'reuters_desktop_native_1'
+    | 'reuters_mobile_mpu_1'
+    | 'reuters_sponsorlogo';
 
   /**
    * The unique placement name from FreeStar dashboard.
    * @required
    */
-  export let placementName: PlacementName = 'reuters_desktop_native';
+  export let placementName: PlacementName = 'reuters_desktop_native_1';
 
   /**
    * The unique slot Id from FreeStar dashboard.
@@ -28,11 +30,30 @@
 
   import Block from '../Block/Block.svelte';
 
+  const getAdType = (placementName: PlacementName) => {
+    switch (placementName) {
+      case 'reuters_desktop_leaderboard_atf':
+      case 'reuters_mobile_leaderboard':
+        return 'leaderboard';
+      case 'reuters_sponsorlogo':
+        return 'Sponsorlogo';
+      case 'reuters_mobile_mpu_1':
+        return 'mpu';
+      default:
+        return 'native';
+    }
+  };
+
   onMount(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const adstest = urlParams.get('adstest');
     window.graphicsAdQueue = window.graphicsAdQueue || [];
     window.graphicsAdQueue.push({
       placementName,
       slotId: placementName,
+      type: getAdType(placementName),
+      graphicId: window.location.pathname,
+      adstest,
     });
   });
 </script>
