8adba40f8821f7c1ece66e89101faf7901d449e0
Kuyoshbek <kuyoshbek.mekhmonov@thomsonreuters.com>
2024-02-12 16:31:25 +0500

    rm unnecessary line

--- src/components/AdSlot/adScripts/bootstrap.ts
+++ src/components/AdSlot/adScripts/bootstrap.ts
@@ -7,7 +7,7 @@ const ONETRUST_SCRIPT_ID = '38cb75bd-fbe1-4ac8-b4af-e531ab368caf-test';
 
 export const loadBootstrap = () => {
   (<any>window).freestar = (<any>window).freestar || {};
-  const freestar = (<any>window).freestar || {};
+  const freestar = (<any>window).freestar;
   freestar.debug = true;
   freestar.queue = freestar.queue || [];
   freestar.config = freestar.config || {};
