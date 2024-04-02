646ee4effba01a5f88d886d3ed43c38a00362d2b
Kuyoshbek <kuyoshbek.mekhmonov@thomsonreuters.com>
2024-04-02 22:47:06 +0500

    change script load flow to make it identical to initial implementation

--- src/components/AdSlot/adScripts/loadScript.ts
+++ src/components/AdSlot/adScripts/loadScript.ts
@@ -1,6 +1,14 @@
-export const loadScript = (src: string, onload?: () => void) => {
+interface attributesInterface {
+  onload?: () => void,
+  async?: boolean
+}
+
+export const loadScript = (src: string, attributes?: attributesInterface) => {
+  const { onload, async = true } = attributes || {};
+
   const script = document.createElement('script');
   script.addEventListener('load', onload);
+  script.async = async;
   script.src = src;
   document.head.append(script);
 };
