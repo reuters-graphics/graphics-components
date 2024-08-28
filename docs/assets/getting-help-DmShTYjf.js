3dbfb30c488c96b06ff527a20a0b358407eb0d83
hobbes7878 <jon.r.mcclure@gmail.com>
2024-08-28 10:56:21 +0100

    docs

---
+++ docs/assets/getting-help-DmShTYjf.js
@@ -0,0 +1,14 @@
+import{j as e,p as t}from"./docsPage-CrCNk6vV.js";import{useMDXComponents as r}from"./index-CswL0fy2.js";import{M as i}from"./index-CluuxvkZ.js";import"./index-DwChlhAQ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-D-8MO0q_.js";import"./iframe-SAeDpW7h.js";import"../sb-preview/runtime.js";import"./index-DxKRhftL.js";import"./index-DrFu-skq.js";function n(o){const s={a:"a",em:"em",h1:"h1",h3:"h3",hr:"hr",img:"img",p:"p",strong:"strong",...r(),...o.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{title:"Guides/Getting help",parameters:{...t}}),`
+`,e.jsx(s.p,{children:e.jsx(s.img,{src:"https://graphics.thomsonreuters.com/style-assets/images/logos/reuters-graphics-logo/svg/graphics-logo-color-dark.svg",alt:""})}),`
+`,e.jsx(s.h1,{id:"getting-help",children:"Getting help"}),`
+`,e.jsxs(s.p,{children:["🚨 ",e.jsx(s.strong,{children:"If you're on deadline"}),", talk to your editor who can point you to the nearest developer."]}),`
+`,e.jsxs(s.p,{children:["If you're ",e.jsx(s.em,{children:"off"})," deadline, we use GitHub issues to track things that need fixing up or could be improved. Use one of the links below to create a new issue."]}),`
+`,e.jsx(s.hr,{}),`
+`,e.jsx(s.h3,{id:"is-something-wrong-in-the-code",children:"Is something wrong in the code?"}),`
+`,e.jsxs(s.p,{children:["🏷️ ",e.jsx(s.a,{href:"https://github.com/reuters-graphics/graphics-components/issues/new?labels=%F0%9F%90%9B%20bug&assignees=hobbes7878",rel:"nofollow",children:"Click here"})," to create an issue on GitHub and report any bugs you've found. It's ",e.jsx(s.strong,{children:"very"})," helpful if you also share a link to any repo you're working in where you've spotted the bug."]}),`
+`,e.jsx(s.hr,{}),`
+`,e.jsx(s.h3,{id:"is-something-wrong-in-the-docs",children:"Is something wrong in the docs?"}),`
+`,e.jsxs(s.p,{children:["🏷️ ",e.jsx(s.a,{href:"https://github.com/reuters-graphics/graphics-components/issues/new?labels=%F0%9F%93%9A%20documentation&assignees=hobbes7878",rel:"nofollow",children:"Click here"})," and tell us which docs are incorrect, unclear or could otherwise be improved."]}),`
+`,e.jsx(s.hr,{}),`
+`,e.jsx(s.h3,{id:"have-an-idea-for-a-new-component-or-how-to-improve-an-existing-one",children:"Have an idea for a new component or how to improve an existing one?"}),`
+`,e.jsxs(s.p,{children:["🏷️ ",e.jsx(s.a,{href:"https://github.com/reuters-graphics/graphics-components/issues/new?labels=%F0%9F%9A%80%20new%20feature&assignees=hobbes7878",rel:"nofollow",children:"Click here"})," and tell us more."]})]})}function j(o={}){const{wrapper:s}={...r(),...o.components};return s?e.jsx(s,{...o,children:e.jsx(n,{...o})}):n(o)}export{j as default};
