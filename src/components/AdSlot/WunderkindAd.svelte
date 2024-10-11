7b111affc7384ec3e8ddaffa14681e9de6d345f4
Ahmed Skandar <ahmed.skandar@thomsonreuters.com>
2024-10-11 16:42:38 +0530

    refactor: Remove unnecessary ad slot display code

--- src/components/AdSlot/WunderkindAd.svelte
+++ src/components/AdSlot/WunderkindAd.svelte
@@ -12,16 +12,10 @@
 <svelte:head>
   {#if !isLoggedIn}
     <script data-cfasync="false" type="text/javascript">
-      freestar.config.enabled_slots.push({
+      freestar?.config?.enabled_slots?.push({
         placementName: 'reuters_1x1_bouncex',
         slotId: 'reuters_1x1_bouncex',
       });
     </script>
   {/if}
 </svelte:head>
-
-{#if !isLoggedIn}
-  <div style="text-align: center;" id="reuters_1x1_bouncex">
-    <!-- Ad will be inserted here by Freestar -->
-  </div>
-{/if}
