dd0820f8c2e3efad7ddeb3fe75f29cea15712630
Ahmed Skandar <ahmed.skandar@thomsonreuters.com>
2024-10-19 01:23:13 +0530

    fix: Iframe ad not showing since when script loads, div not targeted

--- src/components/Dianomi/DianomiAd.svelte
+++ src/components/Dianomi/DianomiAd.svelte
@@ -1,18 +1,11 @@
 <script>
   import { onMount } from 'svelte';
-  let scriptLoaded = false;
-
   onMount(() => {
     const script = document.createElement('script');
     script.src = 'https://www.dianomi.com/js/contextfeed.js';
     script.id = 'dianomi_context_script';
-    script.onload = () => {
-      scriptLoaded = true;
-    };
     document.head.appendChild(script);
   });
 </script>
 
-{#if scriptLoaded}
-  <div class="dianomi_context" data-dianomi-context-id="1948"></div>
-{/if}
+<div class="dianomi_context" data-dianomi-context-id="1948"></div>
