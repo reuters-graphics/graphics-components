e747a1a1bacd710b285203444cc977a8b2b18911
Ahmed Skandar <ahmed.skandar@thomsonreuters.com>
2024-10-24 23:35:57 +0530

    docs: Add WunderkindAd component story docs

---
+++ src/components/AdSlot/WunderkindAd.stories.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Story, Meta } from '@storybook/addon-svelte-csf';
+
+  // @ts-ignore raw
+  import adDocs from './stories/docs/wunderkind.md?raw';
+
+  import WunderkindAd from './WunderkindAd.svelte';
+
+  import { withComponentDocs } from './../../docs/utils/withParams';
+
+  const meta = {
+    ...withComponentDocs(adDocs),
+  };
+</script>
+
+<Meta title="Components/WunderkindAd" component="{WunderkindAd}" {...meta} />
+
+<Story name="Default">
+  <WunderkindAd />
+</Story>
