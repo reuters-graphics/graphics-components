b230e27ac705c66df0e8bc8401b6b557d0c3b79b
MinamiFunakoshiTR <minami.funakoshi@thomsonreuters.com>
2025-03-06 12:16:17 -0800

    updates EndNotes, adds docs

--- src/components/DatawrapperChart/DatawrapperChart.stories.svelte
+++ src/components/DatawrapperChart/DatawrapperChart.stories.svelte
@@ -1,6 +1,6 @@
 <script module lang="ts">
-  import { defineMeta } from '@storybook/addon-svelte-csf';
   import DatawrapperChart from './DatawrapperChart.svelte';
+  import { defineMeta } from '@storybook/addon-svelte-csf';
 
   const { Story } = defineMeta({
     title: 'Components/Graphics/DatawrapperChart',
