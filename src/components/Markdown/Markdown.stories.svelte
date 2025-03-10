f1020c6161675711b01f7a8e7482a0472275d551
MinamiFunakoshiTR <minami.funakoshi@thomsonreuters.com>
2025-03-10 11:27:28 -0700

    set up markdown

--- src/components/Markdown/Markdown.stories.svelte
+++ src/components/Markdown/Markdown.stories.svelte
@@ -1,35 +1,16 @@
 <script module lang="ts">
-  // @ts-ignore raw
-  import componentDocs from './stories/docs/component.md?raw';
-
+  import { defineMeta } from '@storybook/addon-svelte-csf';
   import Markdown from './Markdown.svelte';
 
-  import { withComponentDocs } from '$lib/docs/utils/withParams.js';
-
-  export const meta = {
+  const { Story } = defineMeta({
     title: 'Components/Text elements/Markdown',
     component: Markdown,
-    ...withComponentDocs(componentDocs),
-  };
+  });
 </script>
-
-<script>
-  import { Template, Story } from '@storybook/addon-svelte-csf';
-
-  import Block from '../Block/Block.svelte';
-</script>
-
-<Template >
-  {#snippet children({ args })}
-    <Block>
-      <Markdown {...args} />
-    </Block>
-  {/snippet}
-</Template>
 
 <Story
-  name="Default"
-  args="{{
-    source: 'This is *some* text in **markdown**.',
-  }}"
+  name="Demo"
+  args={{
+    source: 'My *awesome* text in **markdown**.',
+  }}
 />
