7431cda546b191103c7444e8859dcc23f4acda0b
hobbes7878 <Jon.McClure@thomsonreuters.com>
2025-03-12 12:40:53 +0000

    docs

--- src/components/SEO/SEO.stories.svelte
+++ src/components/SEO/SEO.stories.svelte
@@ -12,15 +12,12 @@
 <Story
   name="Demo"
   args={{
-    baseUrl: 'https://graphics.reuters.com',
-    // pageUrl:{new URL(
-    //   'https://graphics.reuters.com/world-coronavirus-tracker-and-maps/'
-    // )}
+    baseUrl: 'https://www.reuters.com',
     seoTitle: 'A title for Google',
     seoDescription: 'A description for Google',
     shareTitle: 'A title for Twitter/Facebook',
     shareDescription: 'A description for Twitter/Facebook',
     shareImgPath:
-      'https://graphics.reuters.com/world-coronavirus-tracker-and-maps/assets/images/share.jpg',
+      'https://www.reuters.com/graphics/world-coronavirus-tracker-and-maps/assets/images/share.jpg',
   }}
 />
