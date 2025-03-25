3c4582bb6af5efd3c8af3972c9dfe59f1ef6f5bd
MinamiFunakoshiTR <minami.funakoshi@thomsonreuters.com>
2025-03-25 09:46:50 -0700

    search bar

--- src/components/SearchInput/SearchInput.stories.svelte
+++ src/components/SearchInput/SearchInput.stories.svelte
@@ -1,26 +1,11 @@
 <script module lang="ts">
-  // @ts-ignore raw
-  import componentDocs from './stories/docs/component.md?raw';
-
+  import { defineMeta } from '@storybook/addon-svelte-csf';
   import SearchInput from './SearchInput.svelte';
 
-  import { withComponentDocs } from '$lib/docs/utils/withParams.js';
-
-  export const meta = {
+  const { Story } = defineMeta({
     title: 'Components/Controls/SearchInput',
     component: SearchInput,
-    ...withComponentDocs(componentDocs),
-  };
+  });
 </script>
 
-<script>
-  import { Template, Story } from '@storybook/addon-svelte-csf';
-</script>
-
-<Template >
-  {#snippet children({ args })}
-    <SearchInput {...args} />
-  {/snippet}
-</Template>
-
-<Story name="Default" args="{{}}" />
+<Story name="Demo" />
