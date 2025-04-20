bb99b6dcedae2c799dfd44ba7a439b198a928c8f
hobbes7878 <Jon.McClure@thomsonreuters.com>
2025-04-20 12:49:58 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig, type UserConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import path from 'path';
 
 const config: UserConfig = defineConfig({
-  resolve: {
-    alias: {
-      '@reuters-graphics/graphics-components': path.resolve('./src'),
-      $lib: path.resolve('./src'),
-      $docs: path.resolve('./src/docs'),
-    },
-  },
   css: {
     preprocessorOptions: { scss: { quietDeps: true, api: 'modern-compiler' } },
   },
