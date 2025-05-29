97476d6a468807bcc7aa9e26ae23c003403b68cc
MinamiFunakoshiTR <minami.funakoshi@thomsonreuters.com>
2025-05-29 12:53:27 -0700

    adds demo text to visible demo

--- src/components/Visible/demo/VisibleDemo.svelte
+++ src/components/Visible/demo/VisibleDemo.svelte
@@ -3,11 +3,13 @@
   import BodyText from '../../BodyText/BodyText.svelte';
   import Block from '../../Block/Block.svelte';
 
+  const demoText =
+    'Adjust the window dimension to see what happens when the HTML element wrapped in `Visible` becomes enters the viewport.';
   const text =
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 </script>
 
-<BodyText {text} />
+<BodyText text={`**${demoText}**`} />
 <BodyText {text} />
 <BodyText {text} />
 <BodyText {text} />
