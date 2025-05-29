15f04770bf13fca60a84b7879b581d02b48dbcef
MinamiFunakoshiTR <minami.funakoshi@thomsonreuters.com>
2025-05-29 12:50:06 -0700

    adds visible demo

--- src/components/Visible/Visible.stories.svelte
+++ src/components/Visible/Visible.stories.svelte
@@ -2,6 +2,7 @@
   import { defineMeta } from '@storybook/addon-svelte-csf';
 
   import Visible from './Visible.svelte';
+  import VisibleDemo from './demo/VisibleDemo.svelte';
 
   const { Story } = defineMeta({
     title: 'Components/Utilities/Visible',
@@ -10,14 +11,5 @@
 </script>
 
 <Story name="Demo">
-  <!-- Optionally set your own `top`, `bottom`, `right` and `left` values with units -->
-  <Visible top={{ value: 10, unit: 'px' }}>
-    {#snippet children(visible)}
-      {#if visible}
-        <p>Visible!</p>
-      {:else}
-        <p>Not yet visible.</p>
-      {/if}
-    {/snippet}
-  </Visible>
+  <VisibleDemo />
 </Story>
---
+++ src/components/Visible/demo/VisibleDemo.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import Visible from '../Visible.svelte';
+  import BodyText from '../../BodyText/BodyText.svelte';
+  import Block from '../../Block/Block.svelte';
+
+  const text =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+</script>
+
+<BodyText {text} />
+<BodyText {text} />
+<BodyText {text} />
+<BodyText {text} />
+
+<Block>
+  <Visible top={{ value: 10, unit: 'px' }}>
+    {#snippet children(visible)}
+      {#if visible}
+        <h2>Visible!</h2>
+      {:else}
+        <h2>Not yet visible.</h2>
+      {/if}
+    {/snippet}
+  </Visible>
+</Block>
+
+<BodyText {text} />
+<BodyText {text} />
+<BodyText {text} />
+<BodyText {text} />
+
+<style lang="scss">
+</style>
