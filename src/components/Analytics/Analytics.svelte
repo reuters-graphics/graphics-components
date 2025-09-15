fde8f30fb5b24dc3caa2eaee1b5f22e2098d781e
paulalexandrescuTR <52189864+paulalexandrescuTR@users.noreply.github.com>
2025-09-15 15:49:15 -0400

    Update Analytics.svelte
    moved GTM script into its own const

--- src/components/Analytics/Analytics.svelte
+++ src/components/Analytics/Analytics.svelte
@@ -3,6 +3,7 @@
 <script module>
   // GTM configuration
   const GTM_ID = 'GTM-P9TTSWG2';
+  const GTM_SCRIPT=`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`
 
   import { registerPageview as registerChartbeatPageview } from './providers/chartbeat';
   import { registerPageview as registerGAPageview } from './providers/ga';
@@ -40,11 +41,7 @@
 
 <svelte:head>
   <!-- Google Tag Manager -->
-  {@html `<${'script'}>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${GTM_ID}');</script>`}
+  {@html `<${'script'}>${GTM_SCRIPT}</script>`}
   <!-- End Google Tag Manager -->
 </svelte:head>
 
