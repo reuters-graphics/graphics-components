6cf47dc7eef9d006b95b4d9f80d30cd28a7c4c8e
hobbes7878 <Jon.McClure@thomsonreuters.com>
2025-09-17 17:14:07 +0100

    improves Google Tag Manager implementation

---
+++ src/components/Analytics/GTM.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  const GTM_ID = 'GTM-P9TTSWG2';
+</script>
+
+<svelte:head>
+  <!-- Google Tag Manager -->
+  <link href="https://www.googletagmanager.com" rel="preconnect" />
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      'gtm.start': new Date().getTime(),
+      event: 'gtm.js',
+    });
+  </script>
+  <script async src={`https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`}>
+  </script>
+  <!-- End Google Tag Manager -->
+</svelte:head>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript>
+  <iframe
+    src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+    height="0"
+    width="0"
+    style="display:none;visibility:hidden"
+    title=""
+  ></iframe>
+</noscript>
+<!-- End Google Tag Manager (noscript) -->
