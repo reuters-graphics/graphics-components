6b9cb80f33ddcc7432e421cc2f73261d0e97cf6a
hobbes7878 <Jon.McClure@thomsonreuters.com>
2025-09-18 09:04:38 +0100

    cleanup

--- src/components/Analytics/providers/ga.ts
+++ src/components/Analytics/providers/ga.ts
@@ -1,13 +1,31 @@
+// Reuters Google Tag ID
+const GOOGLE_TAG_ID = 'G-WBSR7WLTGD';
+
 export default () => {
-  registerPageview();
+  try {
+    window.dataLayer = window.dataLayer || [];
+    if (!window.gtag) {
+      /** @type {Gtag.Gtag} */
+      window.gtag = function () {
+        // eslint-disable-next-line prefer-rest-params
+        window.dataLayer.push(arguments);
+      };
+      window.gtag('js', new Date());
+      // config event registers a pageview by default
+      window.gtag('config', GOOGLE_TAG_ID, {
+        send_page_view: false,
+      });
+      registerPageview();
+    }
+  } catch (e) {
+    console.warn(`Error initialising Google Analytics: ${e}`);
+  }
 };
 
 export const registerPageview = () => {
-  if (typeof window === 'undefined' || !window.dataLayer) return;
-
-  window.dataLayer.push({
-    event: 'page_view',
-    page_location: window.location.href,
-    page_title: document.title,
+  if (typeof window === 'undefined' || !window.gtag) return;
+  window.gtag('event', 'page_view', {
+    page_location: window.location.origin + window.location.pathname,
+    page_title: document?.title,
   });
 };
